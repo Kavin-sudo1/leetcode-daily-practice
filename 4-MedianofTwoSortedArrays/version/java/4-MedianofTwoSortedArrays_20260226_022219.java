b02a957f007ec3e41502a32368b3125f86c64dc5
Kavin prasath K <kavinprasath065@gmail.com>
2026-02-25 21:22:19 -0500

    Create 4-MedianofTwoSortedArrays

---
+++ 4-MedianofTwoSortedArrays/version/java/4-MedianofTwoSortedArrays_20260226_022219.java
@@ -0,0 +1,16 @@
+// Last updated: 2/26/2026, 2:22:19 AM
+1class Solution {
+2    public String longestCommonPrefix(String[] strs) {
+3        Arrays.sort(strs);
+4        String s1=strs[0];
+5        String s2=strs[strs.length-1];
+6        int index=0;
+7        while(index<s1.length() && index<s2.length()){
+8            if(s1.charAt(index)== s2.charAt(index)) index++;
+9            else break;
+10
+11        }
+12        return s1.substring(0,index);
+13        
+14    }
+15}
